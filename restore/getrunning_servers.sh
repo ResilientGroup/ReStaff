9b0856812b39433156ff88d8048f8252a3a0b1b1
Marcel Juhnke <marrat@marrat.de>
2022-06-28 14:10:29 +0200

    update helper script collection for Pterodactyl game server management

--- restore/getrunning_servers.sh
+++ restore/getrunning_servers.sh
@@ -2,7 +2,9 @@
 # svr=8a7e5144
 . .env
 
-ALL_SERVERS=$(curl --location --request GET 'https://panel.reload.works/api/application/nests/1/eggs/15?include=servers' --header 'Accept: application/json' --header 'Authorization: Bearer '$APPLICATION_TOKEN'' --header 'Content-Type: application/json' | jq -r '.attributes.relationships.servers.data | .[] | .attributes.uuid')
+EGG=31
+
+ALL_SERVERS=$(curl --location --request GET 'https://panel.reload.works/api/application/nests/1/eggs/'${EGG}'?include=servers' --header 'Accept: application/json' --header 'Authorization: Bearer '$APPLICATION_TOKEN'' --header 'Content-Type: application/json' | jq -r '.attributes.relationships.servers.data | .[] | .attributes.uuid')
 
 # get state of servers
 for svr in $ALL_SERVERS; do
--- restore/stopall.sh
+++ restore/stopall.sh
@@ -2,7 +2,9 @@
 svr=8a7e5144
 . .env
 
-ALL_SERVERS=$(curl --location --request GET https://panel.reload.works/api/application/nests/1/eggs/28?include=servers --header 'Accept: application/json' --header 'Authorization: Bearer '$APPLICATION_TOKEN'' --header 'Content-Type: application/json' | jq -r '.attributes.relationships.servers.data | .[] | .attributes.uuid')
+# ALL_SERVERS=$(curl --location --request GET https://panel.reload.works/api/application/nests/1/eggs/28?include=servers --header 'Accept: application/json' --header 'Authorization: Bearer '$APPLICATION_TOKEN'' --header 'Content-Type: application/json' | jq -r '.attributes.relationships.servers.data | .[] | .attributes.uuid')
+
+ALL_SERVERS=$(cat running_servers.txt)
 
 # start all servers
 for svr in $ALL_SERVERS; do
